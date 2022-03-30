48259fa2a63ea7f191ec7ab8185e686d01917e51
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-03-30 16:23:19 +0200

    Add files via upload

--- SolutionNormalHPFS1.R
+++ SolutionNormalHPFS1.R
@@ -1,5 +1,5 @@
-print('file: SolutionNormaHPFS1.R')
-# EXERCISE & SOLUTION: exHPFS (part 1)
+print('file: SolutionNormalHPFS1a.R')
+# EXERCISE & SOLUTION: exHPFSM1603 (part 1)
 # Jaynes (2003, p.~189) remarked that 'any function with a single rounded 
 # maximum raised to a higher and higher power, goes into a Gaussian function'. 
 # $y(x) = \sin x$ in the range $0 \le x \le \pi$ is such a function. 
@@ -13,16 +13,14 @@ yn = dnorm(x,y10Mean,y10Sd)
 # rescale
 yn = yn/max(yn)
 library(latex2exp)
-# png('exHPFSM220226Part1.png',width=16,height=12,units='cm',res=300)
+# png('exHPFSM220226Part1.png',width=16,height=16,units='cm',res=300)
 plot(x,y,type='l',lwd=1,col='black',xlab='x',ylab=NA,
-     xlim=c(0,pi),ylim=c(0,1.05),las=1,lty=2)
+     xlim=c(0,pi),ylim=c(0,1.05),las=1,lty=2,cex.lab=1.5)
+title(ylab=TeX('$(sin(x))^n$'),line=2.3,cex.lab=1.5)
 lines(x,y5,col='magenta',lwd=1,lty=3); lines(x,y10,col='blue',lwd=3)
 lines(x,yn,col='red',lwd=3,lty=4)
-text(0.4,0.6,'n = 1',col='black')
-text(0.75,0.4,'n = 5',col='magenta')
-text(1.3,0.2,'n = 10',col='blue')
-text(pi/2,0.01,'normal approximation',col='red')
-title(ylab=TeX('$(sin(x))^n$'),line=2.5)
+text(0.4,0.6,'n = 1',col='black',cex=1.5)
+text(0.75,0.4,'n = 5',col='magenta',cex=1.5)
+text(1.3,0.2,'n = 10',col='blue',cex=1.5)
+text(pi/2,0.01,'normal approximation',col='red',cex=1.5)
 # dev.off()
-
-
