57644d6b6220ed1f03c38a330e628be51dcd7953
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-03-31 06:43:50 +0200

    Add files via upload

--- SolutionPDsPDFsPoissonMedian.R
+++ SolutionPDsPDFsPoissonMedian.R
@@ -13,12 +13,12 @@ for(k in 1:L){lambdak = lambdaArr[k];
 for(m in 1:M){medianEst[m] = median(rpois(n,lambdak))};
 meanMedian[k] = mean(medianEst)}
 library(latex2exp)
-# png('PoissonMedian220228.png',width=16,height=12,units='cm',res=300)
+# png('PoissonMedian220228.png',width=16,height=16,units='cm',res=300)
 plot(lambdaArr,LB,type='l',lwd=3,col='magenta',xlab=expression(lambda),
      ylab='Median',las=1,cex=0.4,ylim=c(min(LB),max(UB)),lty=2,cex.lab=1.5)
 lines(lambdaArr,UB,lwd=3,col='red',lty=2)
 lines(lambdaArr,meanMedian,lwd=3,col='blue')
 lines(lambdaArr,lambdaArr,lwd=3,col='black',lty=3)
 legend('topleft',legend=c('upper limit','lower limit','MC estimate',TeX('$\\lambda$')),
-       col=c('red','magenta','blue','black'),lty=c(2,2,1,3),lwd=c(3,3,3,3))
+       col=c('red','magenta','blue','black'),lty=c(2,2,1,3),lwd=c(3,3,3,3),cex=1.2)
 # dev.off()
