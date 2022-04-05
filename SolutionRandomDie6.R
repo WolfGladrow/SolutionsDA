f5cc8ee31cfcd8249abe0f704140f8c1b471682d
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-04-05 08:47:17 +0200

    Add files via upload

--- SolutionRandomDie6.R
+++ SolutionRandomDie6.R
@@ -4,6 +4,6 @@ set.seed(1953) # set seed for random number generators
 xrange = seq(1,6)
 hrange = seq(0,6)
 k = sample(x=xrange,10000,replace=TRUE);
-# png('RandomDieExercise160721.png',width=16,height=12,units='cm',res=300)
+# png('RandomDieExercise160721.png',width=16,height=16,units='cm',res=300)
 hist(k,breaks=hrange,col='blue',main='',las=1)
 # dev.off()
