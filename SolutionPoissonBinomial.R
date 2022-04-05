5ba4510d0e9f2a9627092364a054f70b0c05f73e
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-04-05 14:44:50 +0200

    Add files via upload

--- SolutionPoissonBinomial.R
+++ SolutionPoissonBinomial.R
@@ -3,7 +3,7 @@ print('file: SolutionPoissonBinomial.R')
 n = 10; k = seq(0,n); p = 0.27; pk = dbinom(x=k,size=n,prob=p)
 lambda1 = sum(k*pk); lambda1r = round(lambda1,3)
 pkp = dpois(k,lambda=lambda1)
-# png('BinomPoisson160817.png',width=16,height=12,units='cm',res=300)
+# png('BinomPoisson160817.png',width=16,height=16,units='cm',res=300)
 plot(k,pk,type='p',lwd=4,col='black',xlab='k',ylim=c(0,max(c(pkp,pk))),
      ylab='Probabilities',las=1,cex=0.6,cex.lab=1.5)
 points(k,pkp,col='magenta',lwd=3,cex=0.6,pch=24)
