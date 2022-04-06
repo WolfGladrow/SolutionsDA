81ec1f4eb401161a35ace36063aa850415a37262
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-04-06 09:47:08 +0200

    Add files via upload

--- SolutionPointEstV2.R
+++ SolutionPointEstV2.R
@@ -11,14 +11,15 @@ NOE = seq(0,7)               # number of events
 NSq = sum(f)      # number of square
 NOB = sum(NOE*f)  # number of bombs
 r = NOB/NSq       # mean rate (estimate)
+lambdaEst = round(r,2)
 print('Predict frequencies based on Poisson distribution:')
 fP = NSq*dpois(NOE,r)
-# png('PoissonV2London180107.png',width=16,height=12,units='cm',res=300)
-plot(NOE,log10(f),type='p',lwd=3,col='blue',xlab='Number of bombs per square',
-     ylab=NA,las=1,cex=1,cex.lab=1.5)
+# png('PoissonV2London180107.png',width=16,height=16,units='cm',res=300)
+plot(NOE,log10(f),type='p',lwd=4,col='black',xlab='Number of bombs per square',
+     ylab=NA,las=1,cex=0.6,cex.lab=1.5,ylim=c(-2,2.5))
 title(ylab=TeX('$log_{10}(frequency)$'),line=2.3,cex.lab=1.5)
-points(NOE,log10(fP),lwd=3,col='red',cex=0.4,pch=24)
-text(0.5,0.2,'mean rate = 0.93',col='red',cex=1.5,pos=4)
+points(NOE,log10(fP),lwd=4,col='magenta',cex=0.6,pch=24)
+text(0.5,0.2,pos=4,bquote(~hat(lambda) == .(lambdaEst)),col='magenta',cex=1.5)
 # dev.off()
 # ---------------------------------------------------
 # Results: 
