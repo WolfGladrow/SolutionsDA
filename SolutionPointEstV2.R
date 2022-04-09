5d4464b84799682f371286d508b4d42d5817eea4
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-04-09 12:09:15 +0200

    Add files via upload

--- SolutionPointEstV2.R
+++ SolutionPointEstV2.R
@@ -12,21 +12,19 @@ NSq = sum(f)      # number of square
 NOB = sum(NOE*f)  # number of bombs
 r = NOB/NSq       # mean rate (estimate)
 lambdaEst = round(r,2)
+# Bayesian estimate:
+n = sum(f)
+s = sum(NOE*f)
+lambdaEstB = (s+1)/n; print(c(round(lambdaEstB,4),'lambdaEstB'))
+ulambdaEstB = sqrt((s+1))/n; print(c(round(ulambdaEstB,4),'ulambdaEstB'))
+lambdaEstBr = round(lambdaEstB,3); ulambdaEstBr = round(ulambdaEstB,3)
 print('Predict frequencies based on Poisson distribution:')
-fP = NSq*dpois(NOE,r)
+fP = NSq*dpois(NOE,lambdaEstB)
 # png('PoissonV2London180107.png',width=16,height=16,units='cm',res=300)
 plot(NOE,log10(f),type='p',lwd=4,col='black',xlab='Number of bombs per square',
      ylab=NA,las=1,cex=0.6,cex.lab=1.5,ylim=c(-2,2.5))
 title(ylab=TeX('$log_{10}(frequency)$'),line=2.3,cex.lab=1.5)
 points(NOE,log10(fP),lwd=4,col='magenta',cex=0.6,pch=24)
-text(0.5,0.2,pos=4,bquote(~hat(lambda) == .(lambdaEst)),col='magenta',cex=1.5)
+text(0.5,-1,pos=4,bquote(~bar(x) == .(lambdaEst)),col='magenta',cex=1.5)
+text(0.5,0,pos=4,bquote(~hat(lambda) == .(lambdaEstBr)  %+-% .(ulambdaEstBr)),col='magenta',cex=1.5)
 # dev.off()
-# ---------------------------------------------------
-# Results: 
-print(c('number of bombs (Clarke, 1946) = ',NOBC))
-print(c('number of bombs (calculated)   = ',NOB1))
-print(c('number of bombs     = ',NOB))
-print(c('number of squares   = ',NSq))
-print(c('mean rate (estimate)   = ',round(r,4)))
-print(c('frequencies (observed) = ',f))
-print(c('frequencies (predicted) = ',round(fP,3)))
