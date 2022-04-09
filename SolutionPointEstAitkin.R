55990719dd3981daf3484aef0c5127223ce7fd05
WolfGladrow <99959205+WolfGladrow@users.noreply.github.com>
2022-04-09 11:56:36 +0200

    Add files via upload

--- SolutionPointEstAitkin.R
+++ SolutionPointEstAitkin.R
@@ -3,7 +3,14 @@ print('file: SolutionPointEstAitkin.R')
 ja = seq(0,8)                    # number of events
 frequencies=c(23,56,88, 95,73,40,17,5,3)   # frequencies
 lambdaEst=sum(ja*frequencies)/sum(frequencies) # estimate of mean rate
-pPredict=dpois(ja,lambdaEst) # probabilities based on estimated lambda
+# Bayesian estimate:
+n = sum(frequencies)
+s = sum(ja*frequencies)
+lambdaEstB = (s+1)/n; print(c(round(lambdaEstB,4),'lambdaEstB'))
+ulambdaEstB = sqrt((s+1))/n; print(c(round(ulambdaEstB,4),'ulambdaEstB'))
+lambdaEstBr = round(lambdaEstB,3); ulambdaEstBr = round(ulambdaEstB,3)
+# predictions:
+pPredict=dpois(ja,lambdaEstB) # probabilities based on estimated lambda
 relfre=frequencies/sum(frequencies) # relative frequencies
 fPredict=pPredict*sum(frequencies)  # predicted frequencies
 # ---------- plots:
@@ -14,7 +21,8 @@ if (sflag == 1) {
        main='',lwd=4,cex=0.6,las=1,cex.lab=1.5)
   title(ylab='Frequency',line=2.5,cex.lab=1.5)
   points(ja,fPredict,col='magenta',lwd=4,cex=0.6,pch=24)
-  text(5.6,80,pos=4,bquote(~hat(lambda) == .(lambdaEst)),col='magenta',cex=1.5)
+  text(2,10,pos=4,bquote(~bar(x) == .(lambdaEst)),col='magenta',cex=1.5)
+  text(2,20,pos=4,bquote(~hat(lambda) == .(lambdaEstBr)  %+-% .(ulambdaEstBr)),col='magenta',cex=1.5)
   # dev.off()
 }
 if (sflag == 2) {
@@ -23,17 +31,7 @@ if (sflag == 2) {
        ylab='',lwd=4,cex.lab=1.5)
   title(ylab='Probability, relative frequency',line=2.8,cex.lab=1.5)
   points(ja,pPredict,col='magenta',lwd=4,cex=0.6,pch=24)
-  text(5.6,0.2,pos=4,bquote(~hat(lambda) == .(lambdaEst)),col='magenta',cex=1.5)
+  text(2,0.025,pos=4,bquote(~bar(x) == .(lambdaEst)),col='magenta',cex=1.5)
+  text(2,0.05,pos=4,bquote(~hat(lambda) == .(lambdaEstBr)  %+-% .(ulambdaEstBr)),col='magenta',cex=1.5)
   # dev.off()
 }
-# ---------------------------------------------------
-# Results: 
-TNOC = sum(frequencies)        # total number of cases
-TNOD = sum(ja*frequencies)     # total number of deaths
-print(c('total number of cases  = ',TNOC))
-print(c('total number of deaths = ',TNOD))
-print(c('estimate of mean rate  = ',lambdaEst))
-print(c('probabilities based on estimated lambda  = ',pPredict))
-print(c('probabilities based on estimated lambda  = ',round(pPredict,4)))
-print(c('relative frequencies  = ',round(relfre,4)))
-print(c('predicted frequencies = ',round(fPredict,4)))
